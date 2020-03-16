650f1cb2c996a0046182ea893c0fb72e8227e95c
Tim Malstead <timmmalstead@gmail.com>
2020-03-15 17:25:39 -0700

    optimizes single route setup

--- server.js
+++ server.js
@@ -4,44 +4,7 @@ const fetch = require("node-fetch")
 let maxNumCached = 0
 let responsesCached = []
 
-express.get("/loop/:requestNum", async (req, res) => {
-  try {
-    const begin = Date.now()
-
-    const requestNum = parseInt(req.params.requestNum)
-
-    if (maxNumCached >= requestNum) {
-      return res.json({
-        requestTime: `${(Date.now() - begin) / 1000} seconds`,
-        posts: responsesCached.slice(0, requestNum)
-      })
-    }
-
-    const returnedPosts = []
-
-    for (let i = maxNumCached + 1; i <= requestNum; i++) {
-      returnedPosts.push(
-        await fetch(`https://jsonplaceholder.typicode.com/photos/${i}`, {
-          method: "GET",
-          credentials: "include",
-          headers: { "Content-Type": "application/json" }
-        }).then(promise => promise.json())
-      )
-    }
-
-    maxNumCached = requestNum
-    responsesCached = [...responsesCached, ...returnedPosts]
-
-    return res.json({
-      requestTime: `${(Date.now() - begin) / 1000} seconds`,
-      posts: responsesCached
-    })
-  } catch (err) {
-    return res.json({ error: err.message })
-  }
-})
-
-express.get("/promise/:requestNum", async (req, res) => {
+express.get("/:requestNum", async (req, res) => {
   try {
     const begin = Date.now()
 
